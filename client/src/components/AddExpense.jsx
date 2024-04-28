408775c878296560394445c7161f79517e140e7a
deepjoy9 <deepjoysarkar9@gmail.com>
2024-04-28 22:12:08 +0530

    feat : Split options with split equally and split manually functionality

--- client/src/components/AddExpense.jsx
+++ client/src/components/AddExpense.jsx
@@ -5,7 +5,7 @@ const AddExpense = ({ toggleModal, groupMembers }) => {
   const [expenseName, setExpenseName] = useState("");
   const [amount, setAmount] = useState("");
   const [paidBy, setPaidBy] = useState("");
-  const [splitEqually, setSplitEqually] = useState(false);
+  const [splitEqually, setSplitEqually] = useState(true);
   const [splitManuallyEnabled, setSplitManuallyEnabled] = useState(false);
   const [splitManually, setSplitManually] = useState([
     { member: "", amount: "", description: "" },
@@ -14,6 +14,8 @@ const AddExpense = ({ toggleModal, groupMembers }) => {
   const [membersInvolved, setMembersInvolved] = useState(groupMembers);
   const [showMembersAccordion, setShowMembersAccordion] = useState(false);
   const [selectOption, setSelectOption] = useState("everyone");
+
+  const [splitOption, setSplitOption] = useState("equally");
 
   const handleSubmitForm = (e) => {
     e.preventDefault();
@@ -37,7 +39,7 @@ const AddExpense = ({ toggleModal, groupMembers }) => {
     setExpenseName("");
     setAmount("");
     setPaidBy("");
-    setSplitEqually(false);
+    setSplitEqually(true);
     setSplitManuallyEnabled(false);
     setSplitManually([{ member: "", amount: "", description: "" }]);
     toggleModal();
@@ -67,7 +69,7 @@ const AddExpense = ({ toggleModal, groupMembers }) => {
     }
   };
 
-  const handleManualSplitOnChange = (index, field, value) => {
+  const handleManualSplitOnChange = (index, field, value, split) => {
     const updatedSplit = [...splitManually];
     updatedSplit[index][field] = value;
     setSplitManually(updatedSplit);
@@ -75,13 +77,22 @@ const AddExpense = ({ toggleModal, groupMembers }) => {
     // Automatically add a new manual split field if the last field has both member and amount selected
     if (
       index === splitManually.length - 1 &&
-      value !== "" &&
+      splitManually[index].member !== "" &&
       splitManually[index].amount !== ""
     ) {
       setSplitManually([
         ...splitManually,
         { member: "", amount: "", description: "" },
       ]);
+    }
+  };
+
+  const handleSplitChange = (event) => {
+    const value = event.target.value;
+    setSplitOption(value);
+    if (value === "manually") {
+      setSplitEqually(false);
+      setSplitManuallyEnabled(true);
     }
   };
 
@@ -178,87 +189,78 @@ const AddExpense = ({ toggleModal, groupMembers }) => {
             )}
           </div>
 
-          {/* Split Equally */}
-          <div className="split-equally-container">
-            <label htmlFor="split-equally">Split Equally :</label>
-            <input
-              type="checkbox"
-              id="split-equally"
-              name="split-equally"
-              checked={splitEqually}
-              onChange={(e) => setSplitEqually(e.target.checked)}
-              disabled={splitManuallyEnabled}
-            />
-          </div>
-
-          {/* Split Manually */}
-          <div className="split-manually-container">
-            <div className="split-manually-label-and-checkbox">
-              <label htmlFor="split-manually">Split Manually :</label>
-              <input
-                type="checkbox"
-                id="split-manually"
-                name="split-manually"
-                checked={splitManuallyEnabled}
-                onChange={(e) => setSplitManuallyEnabled(e.target.checked)}
-                disabled={splitEqually}
-              />
+          {/* Split Options */}
+          <div>
+            <div className="split-options-container">
+              <label htmlFor="split-options">Split Options :</label>
+              <select
+                value={splitOption}
+                onChange={handleSplitChange}
+                id="split-options"
+              >
+                <option value="equally">Split Equally</option>
+                <option value="manually">Split Manually</option>
+              </select>
             </div>
-            {splitManuallyEnabled && (
-              <div className="manual-split-fields">
-                {splitManually.map((split, index) => (
-                  <div key={index} className="manual-split-item">
-                    <select
-                      value={split.member}
-                      onChange={(e) =>
-                        handleManualSplitOnChange(
-                          index,
-                          "member",
-                          e.target.value
-                        )
-                      }
-                    >
-                      <option value="">Select member</option>
-                      {groupMembers.map((member, idx) => (
-                        <option key={idx} value={member}>
-                          {member}
-                        </option>
-                      ))}
-                    </select>
-                    <input
-                      type="text"
-                      value={split.amount}
-                      onChange={(e) =>
-                        handleManualSplitOnChange(
-                          index,
-                          "amount",
-                          e.target.value
-                        )
-                      }
-                      placeholder="Amount"
-                    />
-                    <input
-                      type="text"
-                      value={split.description}
-                      onChange={(e) =>
-                        handleManualSplitOnChange(
-                          index,
-                          "description",
-                          e.target.value
-                        )
-                      }
-                      placeholder="Description"
-                    />
-                    {splitManually.length > 1 && ( // Render remove button only if there are more than one fields
-                      <button
-                        type="button"
-                        onClick={() => handleRemoveManualSplit(index)}
+
+            {/* Split Manually */}
+            {splitOption === "manually" && (
+              <div className="split-manually-container">
+                <div className="manual-split-fields">
+                  {splitManually.map((split, index) => (
+                    <div key={index} className="manual-split-item">
+                      <select
+                        value={split.member}
+                        onChange={(e) =>
+                          handleManualSplitOnChange(
+                            index,
+                            "member",
+                            e.target.value
+                          )
+                        }
                       >
-                        Remove
-                      </button>
-                    )}
-                  </div>
-                ))}
+                        <option value="">Select member</option>
+                        {groupMembers.map((member, idx) => (
+                          <option key={idx} value={member}>
+                            {member}
+                          </option>
+                        ))}
+                      </select>
+                      <input
+                        type="text"
+                        value={split.amount}
+                        onChange={(e) =>
+                          handleManualSplitOnChange(
+                            index,
+                            "amount",
+                            e.target.value
+                          )
+                        }
+                        placeholder="Amount"
+                      />
+                      <input
+                        type="text"
+                        value={split.description}
+                        onChange={(e) =>
+                          handleManualSplitOnChange(
+                            index,
+                            "description",
+                            e.target.value
+                          )
+                        }
+                        placeholder="Description"
+                      />
+                      {splitManually.length > 1 && ( // Render remove button only if there are more than one fields
+                        <button
+                          type="button"
+                          onClick={() => handleRemoveManualSplit(index)}
+                        >
+                          Remove
+                        </button>
+                      )}
+                    </div>
+                  ))}
+                </div>
               </div>
             )}
           </div>
