3b6f325051301c63b9495d6c110b2ab67093e56b
deepjoy9 <deepjoysarkar9@gmail.com>
2024-05-24 21:26:16 +0530

    feat: Implement responsive hamburger menu for smaller devices

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -2,15 +2,27 @@ import React, { useState } from "react";
 import { Link } from "react-router-dom";
 
 const Header = () => {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setIsMenuOpen(!isMenuOpen);
+  };
+
   return (
     <header>
       <Link to="/" className="logo">
         Bill Splitter
       </Link>
-      <nav>
+      <nav
+        className={`nav-links ${isMenuOpen ? "open" : ""}`}
+        onClick={toggleMenu}
+      >
         <Link to="/">My Groups</Link>
         <Link to="/friends">My Friends</Link>
       </nav>
+      <div className="hamburger" onClick={toggleMenu}>
+        &#9776;
+      </div>
     </header>
   );
 };
